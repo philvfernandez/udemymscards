f06af672b6e4a0ace16b90be937bc01f3da318c8
Phil Fernandez <philvfernandez@gmail.com>
2023-02-28 15:38:51 -0700

    Added rest service for cards data.  This includes the controller, model and repository code.  I also added the build pack plugin to the pom file.

---
+++ src/main/java/com/philf/ms/cards/Repository/CardsRepository.java
@@ -0,0 +1,13 @@
+package com.philf.ms.cards.Repository;
+
+import com.philf.ms.cards.model.Cards;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CardsRepository extends CrudRepository<Cards, Long> {
+
+    List<Cards> findByCustomerId(int customerId);
+}
--- src/main/java/com/philf/ms/cards/controller/CardsController.java
+++ src/main/java/com/philf/ms/cards/controller/CardsController.java
@@ -1,4 +1,32 @@
 package com.philf.ms.cards.controller;
 
+import com.philf.ms.cards.Repository.CardsRepository;
+import com.philf.ms.cards.model.Cards;
+import com.philf.ms.cards.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CardsController {
+
+    @Autowired
+    private CardsRepository cardsRepository;
+
+    @PostMapping("/myCards")
+    public List<Cards> getCardDetails(@RequestBody Customer customer) {
+        List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
+
+        if(cards != null) {
+            return cards;
+        } else {
+            return null;
+        }
+
+    }
+
+
 }
